c882ced3ea16d6568edb5e70395fb279db8a968a
Claire Billy <claire.billy@epita.fr>
2020-11-23 20:51:10 +0100

    serialization: fixed date and json format

--- sendPortfolio.py
+++ sendPortfolio.py
@@ -1,7 +1,7 @@
 import requests
 import json
 
-url_post_portfolio = "https://dolphin.jump-technology.com:8443/api/v1/portfolio/2201/dyn_amount_compo"
+url_post_portfolio = "https://dolphin.jump-technology.com:8443/api/v1/portfolio/1821/dyn_amount_compo"
 
 #this does not seem to work for now : 404 error
 def getPortfolio(usr: str, pwd: str, base_url: str, portfolio_id: str):
@@ -17,25 +17,25 @@ def getPortfolio(usr: str, pwd: str, base_url: str, portfolio_id: str):
 def createPortfolio(portfolio: list, assets: list):
   assets_list = []
   for ind in portfolio:
-    asset_id = str(assets[ind])
-    asset_dict = { 'asset': asset_id,
-                   'quantity': 1 #this should probably be modified
+    asset_id = assets[ind]
+    asset_dict = { "asset": int(asset_id),
+                   "quantity": 1 #this should probably be modified
                  }
-    portfolio_asset = { 'asset': asset_dict }
+    portfolio_asset = { "asset": asset_dict }
     assets_list.append(portfolio_asset)
   
   #build dict to send
-  dict_values = { '2013-06-14': assets_list } #written in the subject
-  dict_currency = { 'code': 'EUR'}
-  dict_portfolio = { 'label': 'EPITA_PTF_1',
-                     'currency': dict_currency,
-                     'type': 'front', #written in the subject
-                     'values' : dict_values
+  dict_values = { "2016-06-01": assets_list } #written in the subject
+  dict_currency = { "code": "EUR" }
+  dict_portfolio = { "label": "EPITA_PTF_2",
+                     "currency": dict_currency,
+                     "type": "front", #written in the subject
+                     "values" : dict_values
                    }
 
   return dict_portfolio
 
 
 def sendPortfolio(jsonPortfolio: dict, usr:str, pwd:str):
-    req = requests.put(url_post_portfolio, data=jsonPortfolio, auth=(usr, pwd))
-    return req
+  req = requests.put(url_post_portfolio, data=json.dumps(jsonPortfolio), auth=(usr, pwd))
+  return req
